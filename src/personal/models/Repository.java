a0affb794d42aa89abb8b5bf6b535f079d5b2627
Mikhail <mehan@list.ru>
2023-03-13 22:11:53 +0300

    Repo interface ready

--- src/personal/models/Repository.java
+++ src/personal/models/Repository.java
@@ -1,5 +1,11 @@
 package personal.models;
 
-public interface Repository {
+import java.util.List;
 
+public interface Repository {
+    List<Note> getAllNotes();
+    String createNote(Note note);
+
+    public void saveNote(List<Note> notes);
+    public void deleteNote(String noteId, List<Note> notes);
 }
